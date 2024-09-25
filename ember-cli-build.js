aac23faa30589df44feeb0c227ff25523a2cde27
Kaisa Other <kaisamill@gmail.com>
2024-09-25 23:51:26 +0100

    add tailwind.css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,19 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
+  let app = new EmberApp(defaults, {
+    postcssOptions: {
+      compile: {
+        enabled: true,
+        cacheInclude: [/.*\.(css|scss|hbs|html)$/, /.*\/tailwind\.config\.js$/],
+        plugins: [
+          { module: require('postcss-import') }, // If you installed postcss-import
+          require('tailwindcss'),
+          require('tailwindcss')('./tailwind.config.js'), // If you have a Tailwind config file.
+          require('autoprefixer'),
+        ],
+      },
+    },
   });
 
   return app.toTree();
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.hbs',
+    './app/**/*.html',
+    './index.html', // Include the root index.html file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
