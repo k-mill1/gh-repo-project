498e3956e7e887a276f10ad7e92ddfc570de5399
Kaisa Other <kaisamill@gmail.com>
2024-09-27 10:14:21 +0100

    add filtering by language

--- app/controllers/repo-finder.js
+++ app/controllers/repo-finder.js
@@ -14,6 +14,12 @@ export default class RepoFinderController extends Controller {
   token = '';
 
   @tracked
+  usedLanguages = [];
+
+  @tracked
+  selectedLanguage = '';
+
+  @tracked
   repositories = [];
 
   @action
@@ -24,6 +30,38 @@ export default class RepoFinderController extends Controller {
   @action
   updateToken(event) {
     this.token = event.target.value;
+  }
+
+  @dropTask
+  *filterByLanguage(event) {
+    this.selectedLanguage = event.target.value;
+
+    try {
+      const results = yield this.githubApi.searchReposByLanguage(
+        this.orgName,
+        this.token,
+        this.selectedLanguage
+      );
+
+      const repos = results.items;
+
+      if (repos.length) {
+        for (let repo of repos) {
+          const branches = yield this.githubApi.getBranches(
+            this.orgName,
+            repo.name,
+            this.token
+          );
+          repo.branches = branches;
+          repo.branchesCount = branches.length;
+          repo.showBranches = false;
+        }
+      }
+
+      this.repositories = repos;
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
   }
 
   @dropTask
@@ -46,6 +84,15 @@ export default class RepoFinderController extends Controller {
           repo.branches = branches;
           repo.branchesCount = branches.length;
           repo.showBranches = false;
+
+          const primaryRepoLanguage = repo.language;
+
+          if (
+            primaryRepoLanguage &&
+            !this.usedLanguages.includes(primaryRepoLanguage)
+          ) {
+            this.usedLanguages.push(primaryRepoLanguage);
+          }
         }
       }
 
